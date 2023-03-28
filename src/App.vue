fdd9490b4c9ca88b259a9797385ec86457a9835d
Jacques-Sparrow <lucas.fuentes@protonmail.com>
2023-03-28 13:04:13 -0400

    feat: ajout backTotop

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Erp from "./components/Erp.vue";
 import Divider from "./components/Divider.vue";
 import Ecommerce from "./components/Ecommerce.vue";
 import Skills from "./components/Skills.vue";
+import BackToTop from "./components/BackToTop.vue";
 </script>
 
 <template>
@@ -14,4 +15,5 @@ import Skills from "./components/Skills.vue";
   <Divider></Divider>
   <Ecommerce></Ecommerce>
   <Skills id="skills"></Skills>
+  <BackToTop></BackToTop>
 </template>
---
+++ src/components/BackToTop.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button v-if="showButton" @click="scrollToTop">Retour en haut</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showButton: false,
+    };
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    },
+    handleScroll() {
+      this.showButton = window.pageYOffset > 300;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+};
+</script>
+
+<style scoped>
+button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  padding: 10px;
+  background-color: #646cff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: opacity 0.3s ease-in-out;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+</style>
